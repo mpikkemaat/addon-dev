7a792d86a8c6b17a08cd6d89d7d50e40708086f3
marnix <marnix.pikkemaat@etna-ct.com>
2021-12-30 19:48:02 +0100

    added run.sh

--- tunslip6/run.sh
+++ tunslip6/run.sh
@@ -1,14 +1,6 @@
 
-CONFIG_PATH=/data/options.json
+echo "Starting build"
 
-SERIALPORT="$(jq --raw-output '.serialport' $CONFIG_PATH)"
 
-echo "serial port is set to $SERIALPORT"
-
-#dont remove the following lines, they make tunslip accept the hostname
-#do these lines trigger the actual ip lookup?
-echo "Looking up hostname:"
-host $(hostname)
-hostname -i
-
-/tunslip6 -v1 -s $SERIALPORT fd00::1/64
+cd /addons
+make
